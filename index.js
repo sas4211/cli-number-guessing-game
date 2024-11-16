cb8ed3da034333626aa1eb92a4bb765028ddf9c9
sas4211 <sas4211@gmail.com>
2024-11-16 13:43:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import inquirer from 'inquirer';
 // computer will generate a random number
 // user input for guessing number
 // compare user input with computer generated number and show result
+console.log("Welcome To The Game");
 const randomNumber = Math.floor(Math.random() * 10 + 1); /// f you dont want 0 we do value+1
 const answer = await inquirer.prompt([{
         name: "UserGuessTheNumber",
